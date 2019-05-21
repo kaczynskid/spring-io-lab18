fe7537e9ef7a4668f89ee0474b9753cc81aac89e
Darek Kaczyński <kaczynskid@gmail.com>
2019-05-21 09:35:42 +0200

    Added infra scripts

---
+++ infra/elk.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker-compose -p sprio up -d elk
---
+++ infra/grafana.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# see 'Using the Dashboards' at https://hub.docker.com/r/jlachowski/grafana-graphite-statsd/
+docker-compose -p sprio up -d grafana
