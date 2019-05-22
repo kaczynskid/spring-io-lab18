56a6c4fc2d444dd6299fdbd30b6cc13d60bd7bb7
Darek Kaczyński <kaczynskid@gmail.com>
2019-05-22 12:03:28 +0200

    Made demo greet template refreshable

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,11 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.diagnostics.AbstractFailureAnalyzer;
-import org.springframework.boot.diagnostics.FailureAnalysis;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
@@ -17,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -66,14 +66,21 @@ class GreetingController {
 }
 
 @Component
+@AllArgsConstructor
+@EnableConfigurationProperties(GreetingConfig.class)
 class GreetingService {
 
-	@Value("${greet.template:Hello %!}")
-	private String template;
+	private final GreetingConfig config;
 
 	Greeting greet(String name) {
-		return new Greeting(String.format(template, name));
+		return new Greeting(String.format(config.getTemplate(), name));
 	}
+}
+
+@Data
+@ConfigurationProperties(prefix = "greet")
+class GreetingConfig {
+	private String template = "Hi %s";
 }
 
 @lombok.Value
