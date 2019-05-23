d2a624328321c3f77c8104ae0712555f8a3a4778
Darek Kaczyński <kaczynskid@gmail.com>
2019-05-23 10:11:24 +0200

    Added Spring Boot Admin Server

--- registry/src/main/java/io/spring/lab/registry/RegistryApplication.java
+++ registry/src/main/java/io/spring/lab/registry/RegistryApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
 @SpringBootApplication
 @EnableEurekaServer
+@EnableAdminServer
 public class RegistryApplication {
 
 	public static void main(String[] args) {
