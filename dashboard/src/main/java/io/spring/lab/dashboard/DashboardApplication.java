d33d577a15ce5b431278e82ec45ea6c0e39227fe
Darek Kaczyński <kaczynskid@gmail.com>
2019-05-23 14:57:54 +0200

    Merged hystrix streams with Turbine

--- dashboard/src/main/java/io/spring/lab/dashboard/DashboardApplication.java
+++ dashboard/src/main/java/io/spring/lab/dashboard/DashboardApplication.java
@@ -1,15 +1,31 @@
 package io.spring.lab.dashboard;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.context.annotation.Bean;
+
+import com.netflix.turbine.streaming.servlet.TurbineStreamServlet;
 
 @SpringBootApplication
 @EnableHystrixDashboard
+@EnableTurbine
 public class DashboardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DashboardApplication.class, args);
+		SpringApplication app = new SpringApplication(DashboardApplication.class);
+		app.setAllowBeanDefinitionOverriding(true);
+		app.run(args);
 	}
 
+	// org.springframework.cloud.netflix.turbine.TurbineHttpConfiguration
+	@Bean
+	public ServletRegistrationBean<TurbineStreamServlet> turbineStreamServlet(WebEndpointProperties properties) {
+		return new ServletRegistrationBean<>(
+				new TurbineStreamServlet(),
+				properties.getBasePath() + "/turbine.stream");
+	}
 }
