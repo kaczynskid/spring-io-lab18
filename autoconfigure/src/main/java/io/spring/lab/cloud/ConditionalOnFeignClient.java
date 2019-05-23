aa9d3205676abc6c84f535f174acc027971201a2
Darek Kaczyński <kaczynskid@gmail.com>
2019-05-23 12:00:37 +0200

    Added helper annotations for cloud based coditional and composite test configuration

---
+++ autoconfigure/src/main/java/io/spring/lab/cloud/ConditionalOnFeignClient.java
@@ -0,0 +1,15 @@
+package io.spring.lab.cloud;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+
+@Target({ ElementType.TYPE, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@ConditionalOnProperty(name = "feign.client.enabled", havingValue = "true", matchIfMissing = true)
+public @interface ConditionalOnFeignClient {
+
+}
